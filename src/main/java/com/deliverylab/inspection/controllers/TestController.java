1ef32c2f61e29c76271100ca862df884041da3cb
cskhw <51100329+cskhw@users.noreply.github.com>
2022-12-23 14:34:18 +0900

    test: 배포 테스트

--- src/main/java/com/deliverylab/inspection/controllers/TestController.java
+++ src/main/java/com/deliverylab/inspection/controllers/TestController.java
@@ -6,16 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/dev/api/test")
 public class TestController {
 	@GetMapping("/all")
 	public String allAccess() {
-		return "Public Content.";
+		return "Public Content..";
 	}
-	
+
 	@GetMapping("/user")
 	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public String userAccess() {
