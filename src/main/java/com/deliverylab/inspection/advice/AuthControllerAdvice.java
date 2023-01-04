33ff10ce9f99b1a91f9c7bb6350dbcbb6cbaa305
cskhw <51100329+cskhw@users.noreply.github.com>
2023-01-04 21:30:45 +0900

    fix: kafka 환경변수 값 변경

--- src/main/java/com/deliverylab/inspection/advice/AuthControllerAdvice.java
+++ src/main/java/com/deliverylab/inspection/advice/AuthControllerAdvice.java
@@ -9,16 +9,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import com.deliverylab.inspection.exception.ErrorMessage;
+import com.deliverylab.inspection.exception.RoleNotFoundException;
 import com.deliverylab.inspection.exception.TokenRefreshException;
 
 @RestControllerAdvice
-public class TokenControllerAdvice {
+public class AuthControllerAdvice {
 
   @ExceptionHandler(value = TokenRefreshException.class)
   @ResponseStatus(HttpStatus.FORBIDDEN)
   public ErrorMessage handleTokenRefreshException(TokenRefreshException ex, WebRequest request) {
     return new ErrorMessage(
         HttpStatus.FORBIDDEN.value(),
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false));
+  }
+
+  @ExceptionHandler(value = RoleNotFoundException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorMessage handleTokenRefreshException(RoleNotFoundException ex, WebRequest request) {
+    return new ErrorMessage(
+        HttpStatus.BAD_REQUEST.value(),
         new Date(),
         ex.getMessage(),
         request.getDescription(false));
