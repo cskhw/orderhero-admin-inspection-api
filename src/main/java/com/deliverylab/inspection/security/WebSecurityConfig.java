3e6c05a57b967011cf77b306b6c4043106e64b8f
cskhw <51100329+cskhw@users.noreply.github.com>
2023-01-04 20:55:40 +0900

    fix: kafka 환경변수 값 변경

--- src/main/java/com/deliverylab/inspection/security/WebSecurityConfig.java
+++ src/main/java/com/deliverylab/inspection/security/WebSecurityConfig.java
@@ -93,6 +93,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated();
 
     http.authenticationProvider(authenticationProvider());
+
     http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
